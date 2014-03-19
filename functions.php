7c5f5fd12c38a8b465ec49405bb982b2d1ed9c5d
Caleb Clapp <clappc2@gmail.com>
2014-03-18 20:22:47 -0700

    added custom style sheet

--- functions.php
+++ functions.php
@@ -90,6 +90,8 @@ function _s_scripts() {
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_style( '_s-custom-style', get_template_directory_uri() . '/layouts/custom.css');
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
